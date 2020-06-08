cd0ba4ec18a7487240776e75e2a2c2890dfc99ba
Javier Arroyo <arroyoj3@uci.edu>
2020-06-08 14:29:39 -0700

    SearchApi async functionality, useEffect

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://api.yelp.com/v3/businesses/",
+  baseURL: "https://api.yelp.com/v3/businesses",
   headers: {
     Authorization:
       "Bearer pLsBPHlkcqKlrPLtd-MPVx3VtL3cjA8UBBiDxxWbjerRzNMwItfN2qKZ-mhl_oTliDTcqvpSopidrw1re4kjC27vzVvF5yFvItLW5Tn1HZXMwN3U9SDgWf7V5pfIXnYx",
@@ -10,3 +10,4 @@ export default axios.create({
 
 //Authorization needs to be capitalized
 //Bearer needs to be included before the api key is used
+// yelp.get("/search");  is being concatenated to the baseURL
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,9 +1,35 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { StyleSheet, View, Text, ScrollView } from "react-native";
 import { SearchBar } from "../components/SearchBar";
+import yelp from "../api/yelp";
 
 export default function SearchScreen() {
   const [value, setValue] = useState("");
+  const [results, setResults] = useState([]); // results could be renamed "restaurants" or "businesses"
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const searchApi = async (searchValue) => {
+    console.log("hi there");
+    try {
+      const response = await yelp.get("/search", {
+        params: {
+          limit: 50,
+          term: value,
+          location: "Berkeley",
+        },
+      });
+      setResults(response.data.businesses);
+    } catch (error) {
+      setErrorMessage("Sorry there was an error.");
+    }
+  };
+
+  useEffect(() => {
+    searchApi("pasta");
+  }, []);
+
+  //searchApi('pasta')
+  // BAD CODE. everytime the component rerenders that function is being called which clogs up the react DOM resulting in performance issues.
 
   return (
     <ScrollView style={styles.container}>
@@ -11,9 +37,15 @@ export default function SearchScreen() {
         <SearchBar
           value={value}
           onInputChange={(newValue) => setValue(newValue)}
-          onInputSubmit={() => console.log(value)}
+          onInputSubmit={() => searchApi(value)}
         />
-        <Text> {value} </Text>
+        {errorMessage ? (
+          <Text> {errorMessage} </Text>
+        ) : (
+          <Text> We have found {results.length} results.</Text>
+        )}
+        {/* <Text> We have found {results.length} results. </Text>
+        <Text> {errorMessage}</Text> */}
       </View>
     </ScrollView>
   );
